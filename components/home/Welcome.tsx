a06635939325e8e3bcb2ab4155156ba825cc03cb
Cristian Vasquez <cvo6372@gmail.com>
2023-01-02 22:37:40 -0500

    first commit

---
+++ components/home/Welcome.tsx
@@ -0,0 +1,12 @@
+import Banner from 'assets/banner.jpg'
+import Image from "next/image";
+
+const Home = () => {
+    return (
+        <div className="container col-md-12">
+            
+        </div>
+    )
+}
+
+export default Home
---
+++ pages/login.tsx
@@ -0,0 +1,11 @@
+import Login from "../../components/auth/Login";
+
+const login = () => {
+    return (
+        <>
+            <Login API={process.env.API} DOMAIN_API={process.env.DOMAIN_API} />
+        </>
+    )
+}
+
+export default login
